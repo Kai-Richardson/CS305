2e7e40fed1a0856a15e293298e4c95fb2ff5d06a
Kai Richardson <me@kairichardson.us>
2019-08-26 21:55:23 -0700

    Starter Code prelim inventory / item classes

---
+++ HW/HW0/Item.java
@@ -0,0 +1,27 @@
+/**
+ * Item class
+ * Interacts with: Inventory, Main
+ */
+public class Item {
+    private double price;
+    private String name;
+    private int ID; //fullcaps should be reserved for compiler defines/consts, imo
+
+    Item(double price, String name, int ID) {
+        this.price = price;
+        this.name = name;
+        this.ID = ID;
+    }
+
+    public void print() {
+        System.out.print("Item" + getPrice() + "\t" + name + "\t" + getID())
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public int getID() {
+        return this.ID;
+    }
+}
