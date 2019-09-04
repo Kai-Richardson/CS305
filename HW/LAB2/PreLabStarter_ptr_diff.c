3db16f1dd6ca15d2fb17470d23750440382c7e82
Kai Richardson <me@kairichardson.us>
2019-09-04 05:32:15 -0700

    LAB2 gdb init

--- HW/LAB2/PreLabStarter_ptr_diff.c
+++ HW/LAB2/PreLabStarter_ptr_diff.c
@@ -3,12 +3,13 @@
 /* for prelab 2, question 3 */
 
 int array1[20];
-int main() {
+int main()
+{
   int x = 0;
   int array2[30];
-  int *ptr1 = array1; // points to first element (element 0) of array1
-  int *ptr2 = array2; // points to first element of array2
-  int *ptr3 = &array1[3]; // point to element 3 of array1
+  int *ptr1 = array1;      // points to first element (element 0) of array1
+  int *ptr2 = array2;      // points to first element of array2
+  int *ptr3 = &array1[3];  // point to element 3 of array1
   int *ptr4 = &array2[17]; // points to element 17 of array2
 
   printf("ptr2-ptr1 is %d\n", ptr2 - ptr1);
@@ -19,5 +20,4 @@ int main() {
   printf("ptr4-ptr3 is %d\n", ptr4 - ptr3);
 
   return 0;
-
 }
