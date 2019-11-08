e8a5daafd7f9f357a14d001512525bde0ac785f0
Kai Richardson <me@kairichardson.us>
2019-11-08 08:57:25 -0800

    Starting on airport initilization in the LList

--- HW/HW4/main.c
+++ HW/HW4/main.c
@@ -9,12 +9,77 @@
 #define false 0
 #define true 1
 #define MAX_ALLOWED_INPUT 40
+#define MAX_STRINGLEN 50
 
 int main(int argc, char *argv[])
 {
 	int exited = false; //used to keep track if user has prompted exit
 
+	list *llhead = NULL; //Our master llist head
+	list *lltail = NULL; //Keep track of tail for fast addition
+
 	printf("Starting Init\n");
+
+	if (argc > 1)
+	{
+		FILE *fp = fopen(argv[1], "r");
+		if (fp) //file failure check
+		{
+			//All overwritten every time, init here
+			char *tmp_name = malloc(sizeof(char) * MAX_STRINGLEN);
+			char *tmp_city = malloc(sizeof(char) * MAX_STRINGLEN);
+			char *tmp_country = malloc(sizeof(char) * MAX_STRINGLEN);
+			char *tmp_id1 = malloc(sizeof(char) * MAX_STRINGLEN);
+			char *tmp_id2 = malloc(sizeof(char) * MAX_STRINGLEN);
+			float tmp_lat = -9999;
+			float tmp_lng = -9999;
+			int tmp_alt = -9999;
+			char *tmp_zone = malloc(sizeof(char) * MAX_STRINGLEN);
+			char *tmp_dst = malloc(sizeof(char) * MAX_STRINGLEN);
+			char *tmp_dst2 = malloc(sizeof(char) * MAX_STRINGLEN);
+			char *tmp_type = malloc(sizeof(char) * MAX_STRINGLEN);
+			char *tmp_source = malloc(sizeof(char) * MAX_STRINGLEN);
+
+			while (fscanf(fp, "%s %s %s %s %s %f %f %d %s %s %s %s %s", tmp_name, tmp_city, tmp_country, tmp_id1, tmp_id2, &tmp_lat, &tmp_lng, &tmp_alt, tmp_zone, tmp_dst, tmp_dst2, tmp_type, tmp_source) != EOF)
+			{
+				airport *new_air = malloc(sizeof(airport));
+
+				new_air->name = tmp_name;
+				new_air->city = tmp_city;
+				new_air->country = tmp_country;
+				new_air->id1 = tmp_id1;
+				new_air->id2 = tmp_id2;
+				new_air->lat = tmp_lat;
+				new_air->lng = tmp_lng;
+				new_air->alt = tmp_alt;
+				new_air->zone = tmp_zone;
+				new_air->dst = tmp_dst;
+				new_air->dst2 = tmp_dst2;
+				new_air->type = tmp_type;
+				new_air->source = tmp_source;
+
+				if (llhead == NULL)
+				{
+					llhead = malloc(sizeof(list));
+					llhead->value = new_air;
+
+				}
+
+			}
+
+			//Free all temp. holders
+			free(tmp_name);
+			free(tmp_city);
+			free(tmp_country);
+			free(tmp_id1);
+			free(tmp_id2);
+			free(tmp_zone);
+			free(tmp_dst);
+			free(tmp_dst2);
+			free(tmp_type);
+			free(tmp_source);
+		}
+	}
 
 	while (exited == false)
 	{
