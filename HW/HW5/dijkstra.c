8b09815e0117b71b80723fdeb299513801906a89
Kai Richardson <me@kairichardson.us>
2019-11-20 08:26:05 -0800

    HW5 - Tabs on dij

--- HW/HW5/dijkstra.c
+++ HW/HW5/dijkstra.c
@@ -12,27 +12,24 @@ void dijkstra(Graph *g, char *source)
 
 int isEmpty(Graph *g)
 {
-    if (g == NULL)
-        return 1;
+	if (g == NULL)
+		return 1;
 
-    int num_of_verts = g->V;
+	int num_of_verts = g->V;
 
-    for (int i = 0; i < num_of_verts; i++)
-    {
-        if (g->array[i].color == WHITE)
-            return 0;
-    }
+	for (int i = 0; i < num_of_verts; i++)
+	{
+		if (g->array[i].color == WHITE)
+			return 0;
+	}
 
-    return 1;
+	return 1;
 }
 
 int getMin(Graph *g)
 {
-    if (g == NULL)
-        return -1;
+	if (g == NULL)
+		return -1;
 
-    int small_index;
-
-
-
+	int small_index;
 }
